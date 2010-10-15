c4442c7ae170a0d5c0f4dfc9c41c30cb86d84aa4
alvaromartinorive@gmail.com <alvaromartinorive@gmail.com@25ccc82f-c649-0c7d-1fc8-9039459aa492>
2010-10-15 21:44:48 +0000

    Seguimos adaptando el ejemplo que hemos cogido a nuestro proyecto, para ello borramos clases innecesarias

--- trunk/Practica Troncal/src/iso3/pt/model/Test.java
+++ trunk/Practica Troncal/src/iso3/pt/model/Test.java
@@ -56,7 +56,7 @@ public class Test {
 
 	        Session session = sessionFactory.openSession();
 	        Transaction tx = session.beginTransaction();
-	        
+	        /*
 	        Empleado emp1 = (Empleado) session.get(Empleado.class, 111);
 	        System.out.println(emp1);
 	        emp1.setNombre("Asier Perallos Ruiz");
@@ -70,53 +70,25 @@ public class Test {
 	        	Empleado emp2 = iter.next();
 	        	System.out.println(emp2);	
 	        }
-	        
+	        */
 	        tx.commit();
 	        session.close();
 	        System.out.println("Done busquedaPK!");
 		}
 		
-		public void inserciones2(){
-	    	
-	        Session session = sessionFactory.openSession();
-	        Transaction tx = session.beginTransaction();
-	        
-	        Direccion dir1 = new Direccion("Calle1 - Bilbao", 48007);
-	        Direccion dir2 = new Direccion("Calle2 - Portugalete", 48920);
-	        
-	        session.save(dir1);
-	        session.save(dir2);
-	        
-	        Empleado emp1 = (Empleado) session.get(Empleado.class, 111);
-	        Empleado emp2 = (Empleado) session.get(Empleado.class, 222);
-	        
-	        dir1.addEmpleado(emp1);
-	        dir1.addEmpleado(emp2);
-	        
-	        emp1.addDireccion(dir1);
-	        emp2.addDireccion(dir1);
-	        
-	        dir2.addEmpleado(emp1);
-	        emp1.addDireccion(dir2);
-	        
-	        tx.commit();
-	        session.close();
-	        System.out.println("Done inserciones2!");
-	        
-		}
 		
 		public void busquedaCompleja(){
 	    	
 	        Session session = sessionFactory.openSession();
 	        Transaction tx = session.beginTransaction();
-	        
+	        /*
 	        List<Empleado> empleados = session.createQuery("from Empleado as emp where emp.departamento.nombre = 'Servicios - ESIDE' and emp.puesto = 'secretaria'").list();
 	        
 	        for (Iterator<Empleado> iter = empleados.iterator(); iter.hasNext();) {
 	            Empleado emp1 = iter.next();
 	            System.out.println(emp1);
 	        }
-	        
+	        */
 	        tx.commit();
 	        session.close();
 	        System.out.println("Done busquedaCompleja!");
@@ -128,7 +100,7 @@ public class Test {
 	    	
 	        Session session = sessionFactory.openSession();
 	        Transaction tx = session.beginTransaction();
-	        
+	        /*
 	        Empleado emp1 = (Empleado) session.get(Empleado.class, 222);
 	        
 	        Set<Direccion> direcciones = emp1.getDirecciones();
@@ -141,34 +113,12 @@ public class Test {
 	        emp1.getDepartamento().removeEmpleado(emp1);
 	        
 	        session.delete(emp1);
-	        
+	        */
 	        tx.commit();
 	        session.close();
 	        System.out.println("Done borrado!");
 		}
 		
-		public void inserciones3(){
-	    	
-	        Session session = sessionFactory.openSession();
-	        Transaction tx = session.beginTransaction();
-	        
-	        Departamento dep3 = new Departamento("Comercial");
-	        
-	        Externo ext1 = new Externo(555, "Externo1", "profesor", "ext1@unicomer.es", "Iberdrola", "ext1@iberdrola.es", "bla, bla,...");
-	        
-	        ext1.setDepartamento(dep3);
-	        dep3.addEmpleado(ext1);
-	        
-	        session.save(dep3);
-	        session.save(ext1);
-	        
-	        Externo ext = (Externo) session.get(Externo.class, 555);
-	        System.out.println(ext);
-	        
-	        tx.commit();
-	        session.close();
-	        System.out.println("Done inserciones3!");
-		}
 		
 		public void close(){
 	        sessionFactory.close();
@@ -179,10 +129,8 @@ public class Test {
 	    	t1.inserciones1();
 	    	/*
 	    	t1.busquedaPK();
-	    	t1.inserciones2();
 	    	t1.busquedaCompleja();
 	    	t1.borrado();
-	    	t1.inserciones3();
 	    	*/
 	    	t1.close();
 	    }
